8ac2c9da89d9a968b368d25a555eeb729eee26cd
강동희 <dhkang@innogrid.com>
2024-07-19 14:05:12 +0900

    import 경로 수정

--- src/stories/components/select/Select.stories.ts
+++ src/stories/components/select/Select.stories.ts
@@ -1,5 +1,5 @@
 import type { Meta, StoryObj } from "@storybook/react";
-import { Select } from "./Select";
+import { Select } from ".";
 
 // More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
