9360aa9d2f973f017e4030eacd3f042d3e2b46f6
강동희 <dhkang@innogrid.com>
2024-08-27 15:02:52 +0900

    fix/설정 수정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
-/// <reference types="vite/client" />
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import path, { resolve } from "path";
+import path from "path";
 import { fileURLToPath } from "url";
 import { globSync } from "glob";
 import { libInjectCss } from "vite-plugin-lib-inject-css";
@@ -11,31 +10,30 @@ export default defineConfig({
   plugins: [
     react(),
     libInjectCss(),
-    dts({ exclude: ["**/*.stories.tsx", "**/*.stories.ts"] }),
+    dts({
+      exclude: ["**/*.stories.tsx", "**/*.stories.ts"],
+      insertTypesEntry: true,
+    }),
   ],
   build: {
     lib: {
-      entry: resolve(__dirname, "src/main.ts"),
+      entry: "./src/main.ts",
       formats: ["es"],
       name: "index",
       fileName: (format) => `index.${format}.js`,
     },
     rollupOptions: {
-      external: ["react", "react-dom", "react/jsx-runtime"],
+      external: ["react", "react-dom"],
       input: Object.fromEntries(
         globSync([
           "src/components/**/index.tsx",
           "src/hooks/**/index.ts",
           "src/main.ts",
         ]).map((file) => {
-          // This remove `src/` as well as the file extension from each
-          // file, so e.g. src/nested/foo.js becomes nested/foo
           const entryName = path.relative(
             "src",
             file.slice(0, file.length - path.extname(file).length)
           );
-          // This expands the relative paths to absolute paths, so e.g.
-          // src/nested/foo becomes /project/src/nested/foo.js
           const entryUrl = fileURLToPath(new URL(file, import.meta.url));
           return [entryName, entryUrl];
         })
@@ -46,7 +44,6 @@ export default defineConfig({
         globals: {
           react: "React",
           "react-dom": "ReactDOM",
-          "react/jsx-runtime": "react/jsx-runtime",
         },
       },
     },
